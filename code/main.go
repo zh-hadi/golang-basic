6595e83512287a51058fef31bde72238344e925e
Hadiuzzaman <zhhadi50@gmail.com>
2023-01-24 12:26:01 +0600

    Decode example

--- code/main.go
+++ code/main.go
@@ -56,5 +56,11 @@ func foo(w http.ResponseWriter, r *http.Request){
 }
 
 func bar(w http.ResponseWriter, r *http.Request){
+	var p1 person
+	err := json.NewDecoder(r.Body).Decode(&p1)
+	if err != nil {
+		log.Println("Decode bad data", err)
+	}
 
+	log.Println("Decode data: ", p1)
 }
